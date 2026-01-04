4315698af96baa57e5a42ea1177bfaa0c6c652dc
Ben Melling <39852644+benmelling@users.noreply.github.com>
2026-01-04 11:40:25 +0000

    test

--- fibre-tv/src/App.jsx
+++ fibre-tv/src/App.jsx
@@ -1,28 +1,8 @@
-import { useState } from "react";
-import { ThemeProvider, CssBaseline, IconButton } from "@mui/material";
-import SwapHorizIcon from "@mui/icons-material/SwapHoriz";
-
-import { material3 } from "./themes/material3";
-import { liquidGlass } from "./themes/liquidGlass";
-
-import Login from "./screens/Login";
-import Dashboard from "./screens/Dashboard";
-
 export default function App() {
-  const [creds, setCreds] = useState(null);
-  const [glass, setGlass] = useState(false);
-
-  return (
-    <ThemeProvider theme={glass ? liquidGlass : material3}>
-      <CssBaseline />
-      <IconButton
-        onClick={() => setGlass(!glass)}
-        sx={{ position: "fixed", top: 12, right: 12 }}
-      >
-        <SwapHorizIcon />
-      </IconButton>
-
-      {creds ? <Dashboard creds={creds} /> : <Login onLogin={setCreds} />}
-    </ThemeProvider>
-  );
+    return (
+        <div style={{ padding: 40, fontSize: 24 }}>
+              Fibre TV is alive 🚀
+                  </div>
+                    );
+                    }
 }
