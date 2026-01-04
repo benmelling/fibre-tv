6ae4d97c3e714ff386ef34cd094ca286203692f6
Ben Melling <39852644+benmelling@users.noreply.github.com>
2026-01-04 11:34:56 +0000

    fix

--- fibre-tv/src/api/xtream.js
+++ fibre-tv/src/api/xtream.js
@@ -1,9 +1,8 @@
 export async function xtream(creds, action) {
-      const base = creds.server.replace(/\/$/, "");
-        const url = `${base}/player_api.php?username=${creds.username}&password=${creds.password}&action=${action}`;
+  const base = creds.server.replace(/\/$/, "");
+  const url = `${base}/player_api.php?username=${creds.username}&password=${creds.password}&action=${action}`;
 
-          const res = await fetch(url);
-            if (!res.ok) throw new Error("Xtream API error");
-              return res.json();
-              }
+  const res = await fetch(url);
+  if (!res.ok) throw new Error("Xtream API error");
+  return res.json();
 }
