926e91f44671f53f010a2295f9407f2bf5e67715
Ben Melling <39852644+benmelling@users.noreply.github.com>
2026-01-12 14:51:12 +0000

    Delete fibre-tv directory

--- fibre-tv/src/api/xtream.js
+++
@@ -1,8 +0,0 @@
-export async function xtream(creds, action) {
-  const base = creds.server.replace(/\/$/, "");
-  const url = `${base}/player_api.php?username=${creds.username}&password=${creds.password}&action=${action}`;
-
-  const res = await fetch(url);
-  if (!res.ok) throw new Error("Xtream API error");
-  return res.json();
-}
